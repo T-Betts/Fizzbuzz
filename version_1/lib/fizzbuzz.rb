969d7becea86c1eaadd0376dae0203d250d009bc
Tom Betts <tom.betts@live.co.uk>
2018-02-14 23:10:38 +0000

    complete all tests and production code for fizzbuzz

--- version_1/lib/fizzbuzz.rb
+++ version_1/lib/fizzbuzz.rb
@@ -1,3 +1,8 @@
 def fizzbuzz(num)
+  return "Integers only please" if !num.is_a? Integer
+  return 0 if num == 0
+  return "fizzbuzz" if num % 15 == 0
   return "fizz" if num % 3 == 0
+  return "buzz" if num % 5 == 0
+  num
 end
