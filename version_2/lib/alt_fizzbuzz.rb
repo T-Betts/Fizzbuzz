bba4cc8c0a2574d46384c3b019bf50a436a8f1b4
Tom Betts <tom.betts@live.co.uk>
2018-02-15 12:42:45 +0000

    Fix code quality

--- version_2/lib/alt_fizzbuzz.rb
+++ version_2/lib/alt_fizzbuzz.rb
@@ -2,10 +2,10 @@ class Integer
 
   def fizzbuzz
     return 0 if self == 0
-  	return "fizzbuzz" if self % 15 == 0
-  	return "fizz" if self % 3 == 0
-  	return "buzz" if self % 5 == 0
-  	self
+    return "fizzbuzz" if self % 15 == 0
+    return "fizz" if self % 3 == 0
+    return "buzz" if self % 5 == 0
+    self
   end
 
 end
